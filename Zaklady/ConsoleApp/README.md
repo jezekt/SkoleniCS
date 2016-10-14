a958e63d10d26b144cb63716a1c05f949816702e
Tomáš Ježek <jezek.mail@seznam.cz>
2016-10-14 15:56:34 +0200

    Step 2 - Datové typy, proměnné a výrazy

--- Zaklady/ConsoleApp/README.md
+++ Zaklady/ConsoleApp/README.md
@@ -61,6 +61,47 @@ namespace JezekT.SkoleniCS.Zaklady.ConsoleApp
 
 ### Step 2 - Datové typy, proměnné a výrazy
 * přehled datových typů je uveden [zde](https://docs.microsoft.com/cs-cz/dotnet/articles/csharp/tour-of-csharp/types-and-variables);
+* deklarace proměnných
+```c#
+// Explicitní typování
+int x; // deklarace proměnné
+x = 1000000; // naplnění proměnné hodnotou
+byte y = 125; // deklarace proměnné a naplnění hodnotou
+
+// Implicitní typování - o typu rozhodne kompilátor
+var soucet = x + y; // deklarace proměnné a naplnění hodnotou
+```
+* implicitní a explicitní přetypování je popsáno [zde](https://msdn.microsoft.com/cs-cz/library/ms173105.aspx)
+```c#
+int x = 150;
+byte y = 1;
+int soucet = x + y;
+
+// Implicitní přetypování
+long z = x; // implicitní převod typu int (32 bit) na typ long (64 bit)
+//y = x; // nelze provést - nelze zapsat hodnotu typu int (32 bit) do proměnné typu byte (8 bit)
+x = y; // lze provést - proměnná y je typu byte a její hodnotu lze zapsat do proměnné typu int
+
+// Explicitní přetypování
+y = (byte) soucet;
+```
+* převod řetězce na číslo
+```c#
+string text = "text";
+//int i = text; // nelze přetypovat string na typ int
+int i = int.Parse(text); // v tomto případě dojde během přetypování k výjimce
+i = int.Parse("1256"); // řetězec "1256" bude převeden na celé číslo 1256
+
+if (int.TryParse(text, out i))
+{
+	Console.WriteLine("Přetypování se zdařilo.");
+}
+else
+{
+	Console.WriteLine("Nelze přetypovat.");
+}
+```
+
 * výrazy jsou uvedeny [zde](https://docs.microsoft.com/cs-cz/dotnet/articles/csharp/tour-of-csharp/expressions).
 
 ### Step 3 - Výčtové typy
--- Zaklady/ConsoleApp/src/JezekT.SkoleniCS.Zaklady.ConsoleApp/Program.cs
+++ Zaklady/ConsoleApp/src/JezekT.SkoleniCS.Zaklady.ConsoleApp/Program.cs
@@ -6,8 +6,53 @@ namespace JezekT.SkoleniCS.Zaklady.ConsoleApp
     {
         static void Main()
         {
-            Console.WriteLine("Hello world");
-            Console.ReadKey();
         }
+
+        private static void DeklaracePromennych()
+        {
+            // Explicitní typování
+            int x; // deklarace proměnné
+            x = 1000000; // naplnění proměnné hodnotou
+            byte y = 125; // deklarace proměnné a naplnění hodnotou
+
+            // Implicitní typování - o typu rozhodne kompilátor
+            var soucet = x + y; // deklarace proměnné a naplnění hodnotou
+        }
+
+
+        private static void PretypovaniCisel()
+        {
+            int x = 150;
+            byte y = 1;
+            int soucet = x + y;
+
+            // Implicitní přetypování
+            long z = x; // implicitní převod typu int (32 bit) na typ long (64 bit)
+            //y = x; // nelze provést - nelze zapsat hodnotu typu int (32 bit) do proměnné typu byte (8 bit)
+            x = y; // lze provést - proměnná y je typu byte a její hodnotu lze zapsat do proměnné typu int
+
+            // Explicitní přetypování
+            y = (byte) soucet;
+        }
+
+        private static void PretypovaniStringu()
+        {
+            string text = "text";
+            //int i = text; // nelze přetypovat string na typ int
+            int i = int.Parse(text); // v tomto případě dojde během přetypování k výjimce
+            i = int.Parse("1256"); // řetězec "1256" bude převeden na celé číslo 1256
+
+            if (int.TryParse(text, out i))
+            {
+                Console.WriteLine("Přetypování se zdařilo.");
+            }
+            else
+            {
+                Console.WriteLine("Nelze přetypovat.");
+            }
+
+        }
+
+
     }
 }
